64c7206d10074d46daac0b98bff6b4e88729f567
ankush25 <ankushthummar25@gmail.com>
2024-02-16 08:15:29 +0530

    15-2-2024

--- Search/Binary Search/binary_search.c
+++ Search/Binary Search/binary_search.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 
+int a[10]={2,4,6,8,10,12,14,16,18,20};
 int i;
+int mid,first,last;
 
-int main()
+int value()
 {
-    int a[10]={2,4,6,8,10,12,14,16,18,20};
 
     for(i=0 ; i<10 ; i++)
     {
         printf("%d\t",a[i]); 
+    }
+}
+
+int main()
+{
+    if(last >= first)
+    {
+        mid = (last + first)/2;
     }
 }
