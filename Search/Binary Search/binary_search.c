891136a8feb458ea5824f1ebdcae8fccd0f43212
ankush25 <ankushthummar25@gmail.com>
2024-02-16 15:00:53 +0530

    16-02-2024

--- Search/Binary Search/binary_search.c
+++ Search/Binary Search/binary_search.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int a[10]={2,4,6,8,10,12,14,16,18,20};
-int i;
-int mid,first,last;
+int i, mid, min, max,x,n, result;
+
 
 int value()
 {
@@ -13,10 +13,48 @@ int value()
     }
 }
 
+int binarySerch(int n, int min, int max)
+{
+    
+    if (min <= max )
+    {
+        int mid;
+        mid = min + (max - min) / 2;
+
+        if (mid == n)
+        {
+            return mid;
+        }
+        else if (mid < n)
+            {
+                min = mid +1;
+            }
+            else 
+                {
+                     max = mid-1;
+                }
+    }
+    return -1;  
+}
+
 int main()
 {
-    if(last >= first)
+    value();
+
+    printf("\n\nEnter value : ");
+    scanf("%d", &x);
+
+
+    result = binarySerch(n, min, max);
+
+    if (result == 1)
     {
-        mid = (last + first)/2;
+        printf("\nYes it is Present [%d].",a);
     }
+        else
+        {
+             printf("\nNot it is Present.");
+        }
+
+    return 0;
 }
